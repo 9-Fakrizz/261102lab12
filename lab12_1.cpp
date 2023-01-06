29f1922f5e59b662635728494c5f1de6d5c65b15
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-01-06 15:30:52 +0700

    Ask Real?

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   srand(time(0));
+   string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+   int dice = rand() % 9;
+   cout << "Press Enter 3 times to reveal your future." ;
+   cin.get();
+   cin.get();
+   cin.get();
+
+   cout << "You will get "<< grade[dice] <<" in this 261102.";
+}
